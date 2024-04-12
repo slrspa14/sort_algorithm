b93b05db52b837e8a0113d9a5a6e5f4f3ce46db3
slrspa14 <kw32482002@gmail.com>
2024-04-12 17:41:44 +0900

    +3

--- sort_thread_test/sort_thread_test/MainWindow.xaml.cs
+++ sort_thread_test/sort_thread_test/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace sort_thread_test
 {
@@ -14,7 +15,7 @@ namespace sort_thread_test
     {
         int[] sort_data;
         int rank = 1;
-        int sleep = 10;
+        int sleep = 20;
         public MainWindow()
         {
             InitializeComponent();
@@ -22,10 +23,10 @@ namespace sort_thread_test
         void Rand_data()
         {
             Random data = new Random();
-            sort_data = new int[250];
+            sort_data = new int[100];
             for(int i=0; i< sort_data.Length; i++)
             {
-                sort_data[i] = data.Next(0, 1000);
+                sort_data[i] = data.Next(0, 10000);
             }
         }
 
@@ -52,6 +53,7 @@ namespace sort_thread_test
                 sort.Children.Add(rect);
             }
         }
+        
         private void Start_Click(object sender, RoutedEventArgs e)
         {
             Rand_data();
@@ -77,6 +79,26 @@ namespace sort_thread_test
                     Thread quick = new Thread(Quick_start);
                     sort_name.Content = "Quick Sort";
                     quick.Start();
+                    break;
+                case 4:
+                    Thread selection = new Thread(Selection_sort);
+                    sort_name.Content = "Selection Sort";
+                    selection.Start();
+                    break;
+                case 5:
+                    Thread cocktail = new Thread(Cocktail_sort);
+                    sort_name.Content = "Cocktail Sort";
+                    cocktail.Start();
+                    break;
+                case 6:
+                    Thread insertion = new Thread(Insertion_sort);
+                    sort_name.Content = "Insertion Sort";
+                    insertion.Start();
+                    break;
+                case 7:
+                    Thread counting = new Thread(Counting_sort);
+                    sort_name.Content = "Counting Sort";
+                    counting.Start();
                     break;
             }
         }
@@ -139,7 +161,7 @@ namespace sort_thread_test
         }
         void Merge(int[] arr, int left, int mid, int right)
         {
-            int[] sorted = new int[250];
+            int[] sorted = new int[100];
             int i, j, k = left;
             for (i = left, j = mid + 1; i <= mid && j <= right;)
             {
@@ -159,7 +181,7 @@ namespace sort_thread_test
             for (int l = left; l <= right; l++)
                 sort_data[l] = sorted[l];
         }
-        private void Heap_sort()
+        private void Heap_sort()//async
         {
             try
             {
@@ -168,11 +190,11 @@ namespace sort_thread_test
                 // 최대 힙 초기화
                 for (int i = sort_data.Length / 2 - 1; i >= 0; i--)
                 {
-                    heapify(sort_data, sort_data.Length, i);
+                    heapify(sort_data, sort_data.Length, i);//
                 }
                 for (int i = sort_data.Length - 1; i > 0; i--)
                 {
-                    swap(sort_data, 0, i);
+                    Swap(sort_data, 0, i);
                     heapify(sort_data, i, 0);
                     Dispatcher.Invoke(Draw_sort);
                     Thread.Sleep(sleep);
@@ -185,7 +207,7 @@ namespace sort_thread_test
                 MessageBox.Show($"힙 정렬 오류: {ex}");
             }
         }
-        static void heapify(int[] array, int arrayLength, int i)
+        private void heapify(int[] array, int arrayLength, int i)
         {
             int parent = i;
             int left = i * 2 + 1;
@@ -203,16 +225,18 @@ namespace sort_thread_test
             // 부모노드 < 자식노드
             if (i != parent)
             {
-                swap(array, parent, i);
+                Swap(array,parent, i);
+                //Swap(array, parent, i);//
+                
                 heapify(array, arrayLength, parent);
             }
         }
-        static void swap(int[] array, int a, int b)
+        private void Swap(int[] ary, int a, int b)
         {
             int temp;
-            temp = array[a];
-            array[a] = array[b];
-            array[b] = temp;
+            temp = sort_data[a];
+            sort_data[a] = sort_data[b];
+            sort_data[b] = temp;
         }
         void Quick_start()
         {
@@ -272,6 +296,136 @@ namespace sort_thread_test
             return high;
         }
 
+        private void Selection_sort()
+        {
+            try
+            {
+                Stopwatch time = new Stopwatch();
+                time.Start();
+                for (int i = 0; i < sort_data.Length; i++)
+                {
+                    int min = i;
+                    for (int j = i + 1; j < sort_data.Length; j++)
+                    {
+                        if (sort_data[min] > sort_data[j])
+                        {
+                            min = j;
+                        }
+                    }
+                    //if (i != min)
+                    //{
+                        int temp = sort_data[i];
+                        sort_data[i] = sort_data[min];
+                        sort_data[min] = temp;
+                    //}
+                    Dispatcher.Invoke(Draw_sort);
+                    Thread.Sleep(sleep);
+                }
+                time.Stop();
+                Sort_record("Selection", time);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"선택 정렬 오류{ex}");
+            }
+        }
+
+        private void Cocktail_sort()
+        {
+            int left = 0;
+            int right = sort_data.Length - 1;
+            int last = right;
+            //버블정렬 변형
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            while(left < right)
+            {
+                for (int i = left; i < right; i++)
+                {
+                    if(sort_data[i] > sort_data[i+1])
+                    {
+                        int temp = sort_data[i];
+                        sort_data[i] = sort_data[i + 1];
+                        sort_data[i + 1] = temp;
+                        Dispatcher.Invoke(Draw_sort);
+                        Thread.Sleep(sleep);
+                    }
+                }
+                right--;
+                for(int i = right; i> left; i--)
+                {
+                    if(sort_data[i-1] > sort_data[i])
+                    {
+                        int temp = sort_data[i - 1];
+                        sort_data[i - 1] = sort_data[i];
+                        sort_data[i] = temp;
+                        Dispatcher.Invoke(Draw_sort);
+                        Thread.Sleep(sleep);
+                    }
+                }
+                left++;
+            }
+            time.Stop();
+            Sort_record("Cocktail", time);
+        }
+
+        private void Insertion_sort()
+        {
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            for(int i = 1; i < sort_data.Length; i++)
+            {
+                int key = i;
+                for(int j = i-1; j>=0; j--)
+                {
+                    if (sort_data[key] < sort_data[j])
+                    {
+                        int temp = sort_data[j];
+                        sort_data[j] = sort_data[key];
+                        sort_data[key] = temp;
+                        key = j;
+                        Dispatcher.Invoke(Draw_sort);
+                        Thread.Sleep(sleep);
+                    }
+                    else
+                        break;
+                }
+            }
+            time.Stop();
+            Sort_record("Insertion", time);
+
+        }
+        private void Counting_sort()
+        {
+            int max = sort_data.Max();
+            int size = sort_data.Length;
+            int[] output = new int[size];
+            int[] count = new int[max + 1];
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            //size, 최대값+1,메모리 낭비 심한 정렬
+            for (int i = 0; i < max; i++)
+                count[i] = 0;
+            for (int i = 0; i < size; i++)
+                count[sort_data[i]]++;
+            for (int i = 1; i <= max; i++)
+                count[i] += count[i - 1];
+            for(int i=0;i<size;i++)
+            {
+                output[count[sort_data[i]] - 1] = sort_data[i];
+                count[sort_data[i]]--;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                sort_data[i] = output[i];
+                Dispatcher.Invoke(Draw_sort);
+                Thread.Sleep(sleep);
+            }
+            time.Stop();
+            Sort_record("Counting", time);
+        }
+
+
         private void Sort_record(string algorithm, Stopwatch time)
         {
             Dispatcher.Invoke(() =>
@@ -280,6 +434,6 @@ namespace sort_thread_test
             });
             rank++;
         }
-
     }
+    
 }
